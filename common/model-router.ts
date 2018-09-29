c55de346c1054c51ca3df0a5f7c0069131c8f605
peperoschach <luiz.felipe_23@yahoo.com.br>
2018-09-28 23:39:02 -0300

    Add Links to API

--- common/model-router.ts
+++ common/model-router.ts
@@ -11,6 +11,13 @@ export abstract class ModelRouter<D extends mongoose.Document> extends Router {
     return query  
   }
 
+  envelope(document: any): any {
+    let resource = Object.assign({_links:{}}, document.toJSON())
+    resource._links.self = `/${this.model.collection.name}/${resource._id}`
+    console.log(resource)
+    return resource
+  }
+
   validateId = (req, resp, next)=> {
     if(!mongoose.Types.ObjectId.isValid(req.params.id)) {
       next(new NotFoundError('Document no found'))
